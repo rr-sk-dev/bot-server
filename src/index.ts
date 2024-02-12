eee7495baab0edf4f6036fa8704266481fb506b5
rtsrodrigues <ruben.rodrigues@productdock.com>
2024-02-12 18:55:36 +0000

    chore: use env. variables

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { Elysia } from 'elysia';
 import { createWebSocket } from './socket';
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000);
+const port = process.env.SERVER_PORT || 3000;
+
+const app = new Elysia().get('/', () => 'Hello Elysia').listen(port);
 createWebSocket();
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`);
